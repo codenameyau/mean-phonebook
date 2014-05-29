82b1b313369c91e5e19d297f071ff62cd4de6377
Jorge Yau <jyau01@citymail.cuny.edu>
2014-05-28 21:40:05 -0400

    Minor updates

--- app/scripts/controllers/navbarController.js
+++ app/scripts/controllers/navbarController.js
@@ -2,7 +2,7 @@
 
 angular.module('meanPhonebookApp')
   .controller('NavbarCtrl', function ($scope, $location) {
-    $scope.logo = 'Mean Phonebook';
+    $scope.logo = 'MEAN Phonebook';
     $scope.menu = [
       {'icon': 'search', 'link': '/'},
       {'icon': 'plus', 'link': '/add'}
