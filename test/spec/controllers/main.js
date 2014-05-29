8871caa2b5d3ad86ad553ccab1d0a6cde6125698
Jorge Yau <jyau01@citymail.cuny.edu>
2014-05-29 00:09:23 -0400

    Finishing Jasmine tests for HomepageCtrl

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -32,11 +32,29 @@ describe('Controller: HomepageCtrl', function () {
     });
   }));
 
-  it('should attach a list of persons to the scope', function () {
+  it('should attach a list of all persons to the scope', function () {
     expect(scope.allPersons).toBeUndefined();
     $httpBackend.flush();
     expect(scope.allPersons.length).toBe(2);
     expect(scope.allPersons[0].firstName).toEqual('John');
-    expect(scope.allPersons[1].firstName).toEqual('Mary');
+    expect(scope.allPersons[0].lastName).toEqual('Smith');
+    expect(scope.allPersons[1].phoneNumber).toEqual('123-123-5632');
+    expect(scope.allPersons[1].zipcode).toEqual('01102');
   });
+
+  it('should check all items in person should be of type string', function () {
+    expect(scope.allPersons).toBeUndefined();
+    $httpBackend.flush();
+    expect(typeof scope.allPersons[0].firstName).toEqual('string');
+    expect(typeof scope.allPersons[0].lastName).toEqual('string');
+    expect(typeof scope.allPersons[0].phoneNumber).toEqual('string');
+    expect(typeof scope.allPersons[0].zipcode).toEqual('string');
+  });
+
+  it('should expected scope ordering to be by lastName asc', function () {
+    expect(scope.allPersons).toBeUndefined();
+    $httpBackend.flush();
+    expect(scope.ordering).toEqual('lastName');
+  });
+
 });
