b86776772d62b8d320816f324f39098b7b9079a8
Jorge Yau <jyau01@citymail.cuny.edu>
2014-05-30 17:37:58 -0400

    Checking for dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,7 +26,7 @@ module.exports = function (grunt) {
     },
     express: {
       options: {
-        port: process.env.PORT || 9000
+        port: process.env.PORT || 8080
       },
       dev: {
         options: {
